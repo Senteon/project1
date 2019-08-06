54e080f0a9faf5ba6bbd1a3334b59a98341cb44f
Bilal <bilal.u.quraishi@gmail.com>
2019-08-05 17:18:13 -0700

    Update.

--- NumberGuesser.py
+++ NumberGuesser.py
@@ -2,15 +2,15 @@ import random
 
 
 def NumberGuesser():
-    rand = random.randint(0, 100)
+    randomi = random.randint(0, 100)
     guess = -1
-    while guess != rand:
+    while guess != randomi:
         guess = int(input("Type in a any number from 0 to 100."))
-        if rand > guess >= 0:
+        if randomi > guess >= 0:
             print("{} is too low!".format(guess))
-        elif rand < guess <= 100:
+        elif randomi < guess <= 100:
             print("{} is too high!".format(guess))
-        elif guess == rand:
+        elif guess == randomi:
             print("{} is correct!".format(guess))
         else:
             print("{} is an invalid guess. Please try again.".format(guess))
